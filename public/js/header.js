9c01b7c7802edc0a19b13b03f45639ac87547154
asmaebouchrou <asmaebouchrou@gmail.com>
2025-11-24 02:00:03 +0100

    feat(ui): add full redesign for homepage, header, navbar and footer

---
+++ public/js/header.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const menuBtn = document.querySelector(".menu-toggle");
+
+  menuBtn.addEventListener("click", () => {
+    alert("Aquí haremos el menú móvil más adelante ✨");
+  });
+});
